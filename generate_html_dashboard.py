004c506269c4c2aedb9133592cc5241172d25b70
FherStk <porrino.fernando@gmail.com>
2021-05-10 17:52:31 +0200

    Building

--- generate_html_dashboard.py
+++ generate_html_dashboard.py
@@ -1,55 +1,84 @@
 #!/usr/bin/env python
+from random import randrange
 import sys
 
-template = """
+
+course_code = "DAM"
+mp_code = "MPxx"
+mp_name = "Llenguatge de marques"
+
+#TODO: load from BBDD
+legend_text = [
+    "Avalua la metodologia d'aprenentatge, l'organització de la classe i l'assistència rebuda.",
+    "Penses que la manera d'avaluar és l'adequada?",
+    "Penses que el que has après pot ser útil a la teva futura vida professional?",
+    "Penses que el material triat pel professor és l'adequat? (Llibre o apunts, Moodle, activitats, transparències, videotutorials, etc.)"
+]
+
+#fixed colors
+legend_colors = [
+    'rgb(255, 99, 132, 0.25)',
+    'rgb(75, 192, 192, 0.25)',
+    'rgb(255, 205, 86, 0.25)',
+    'rgb(54, 162, 235, 0.25)'    
+]
+
+#more random colors only if needed
+extra_questions = len(legend_text) - len(legend_colors)
+if extra_questions > 0:
+    for i in range(extra_questions):
+        legend_colors.append(f"rgb({randrange(255)}, {randrange(255)}, {randrange(255)}, 0.25)")
+
+
+template = f"""
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Dashboard</title>
+        <title>Dashboard for {mp_code}</title>
 
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jq-3.3.1/jszip-2.5.0/dt-1.10.24/b-1.7.0/b-colvis-1.7.0/b-html5-1.7.0/b-print-1.7.0/cr-1.5.3/kt-2.6.1/r-2.2.7/sp-1.2.2/datatables.min.css"/>
 
         <style>
-            body{
+            body{{
                 background-color: whitesmoke;
                 padding-bottom: 10px;
-            }
+            }}
 
             body, 
-            h1{            
+            h1{{            
                 margin: 0px;
                 font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-            }
+            }}
 
-            header{
+            header{{
                 background-color: #ad0050;
                 color: white;
                 padding: 10px;
-            }
+            }}
 
-            h1{
+            h1{{
                 padding: 0px;
-            }
+            }}
         
-            h2{
+            h2{{
                 font-weight: normal;
-            }
+            }}
 
             .canvas, 
-            .table{
+            .table{{
                 position: relative; 
                 width:100%;          
-            }
+            }}
 
-            .canvas{
+            .canvas{{
                 padding-bottom: 20px;
                 height:400px !important;
-            }
+            }}
 
-            .box{            
+            .box{{            
                 width: calc(50% - 36px);
                 border: solid 1px grey;
                 box-shadow: 1px 1px 10px #888888;
@@ -57,54 +86,54 @@ template = """
                 margin: 10px 10px 0px 10px;
                 padding: 0px 10px 10px 10px;
                 background-color: white;
-            }
+            }}
 
-            .left{
+            .left{{
                 float: left;            
                 margin-right: 5px;
-            }
+            }}
 
-            .right{
+            .right{{
                 float: right;
                 margin-left: 5px;
-            }
+            }}
 
-            .full{
+            .full{{
                 width: calc(100% - 42px);
                 padding: 0px 10px 10px 10px;
-            }
+            }}
 
-            .clear{
+            .clear{{
                 clear: both;
-            }
+            }}
 
-            .content{
+            .content{{
                 width: 100%;
                 display: flex;
-            }
+            }}
 
-            .box h3{
+            .box h3{{
                 margin-top: 10px;
-            }
+            }}
 
-            .box ol{
+            .box ol{{
                 padding-left: 20px;
                 margin-left: 40px;
-            }
+            }}
 
-            .box li{
+            .box li{{
                 cursor: pointer;
-            }
+            }}
 
-            .box li:hover{
+            .box li:hover{{
                 color: darkgray;
-            }
+            }}
 
-            .box li.cross{
+            .box li.cross{{
                 text-decoration: line-through;
-            }
+            }}
 
-            .box li .icon{
+            .box li .icon{{
                 width: 28px;
                 height: 10px;
                 background-color: red;
@@ -112,33 +141,32 @@ template = """
                 display: inline-block;
                 margin-left: -60px;
                 margin-right: 35px;
-            }    
+            }} 
 
             .hide table,
             .hide #data_filter,
             .hide #data_info,
-            .hide #data_paginate{
+            .hide #data_paginate{{
                 display: none;
-            }
+            }}
 
-            .hide .dt-buttons{
+            .hide .dt-buttons{{
                 margin-left: 10px;
-            }   
+            }} 
 
-            @media only screen and (max-width: 800px) {
-                .content{
+            @media only screen and (max-width: 800px) {{
+                .content{{
                     display: inline;
-                }
+                }}
 
                 .left,
-                .right {
+                .right {{
                     float: none;
                     display: block;
                     margin:10px;
                     width: calc(100% - 42px);
-                }
-            }   
-                    
+                }}
+            }}                       
         </style>
 
         <script src="https://cdn.jsdelivr.net/npm/chart.js@3.2.1/dist/chart.min.js"></script>    
@@ -147,8 +175,8 @@ template = """
         <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.3.1/jszip-2.5.0/dt-1.10.24/af-2.3.6/b-1.7.0/b-colvis-1.7.0/b-html5-1.7.0/b-print-1.7.0/cr-1.5.3/kt-2.6.1/r-2.2.7/sp-1.2.2/datatables.min.js"></script>
 
         <script type="text/javascript">
-            window.onload = function () {                       
-                const globalData = {
+            window.onload = function () {{                       
+                const globalData = {{
                     labels:  [
                         "Pregunta 1", 
                         "Pregunta 2", 
@@ -156,7 +184,7 @@ template = """
                         "Pregunta 4", 
                     ],
                     datasets: [
-                        {
+                        {{
                             data:  [9.75, 8.25, 7, 9],
                             backgroundColor: [
                             'rgb(255, 99, 132, 0.25)',
@@ -164,163 +192,162 @@ template = """
                             'rgb(255, 205, 86, 0.25)',
                             'rgb(54, 162, 235, 0.25)'
                             ]
-                        }
+                        }}
                     ]
-                };
+                }};
 
-                const totalData = {
+                const totalData = {{
                     labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                     datasets: [
-                        {
+                        {{
                             label: "Pregunta 1",
                             data:  [0, 0, 0, 1, 0, 2, 7, 5, 3],
                             backgroundColor: 'rgb(255, 99, 132, 0.25)',
                             borderColor: 'rgb(255, 99, 132, 0.25)'
-                        },
-                        {
+                        }},
+                        {{
                             label: "Pregunta 2", 
                             data:  [0, 1, 0, 0, 0, 3, 6, 3, 4],
                             backgroundColor: 'rgb(75, 192, 192, 0.25)',
                             borderColor: 'rgb(75, 192, 192, 0.25)'
-                        },
-                        {
+                        }},
+                        {{
                             label: "Pregunta 3", 
                             data:  [0, 0, 0, 0, 0, 3, 4, 8, 1],
                             backgroundColor: 'rgb(255, 205, 86, 0.25)',
                             borderColor: 'rgb(255, 205, 86, 0.25)'
-                        },
-                        {
+                        }},
+                        {{
                             label: "Pregunta 4",
                             data:  [0, 1, 0,2, 0, 2, 2, 9, 1],
                             backgroundColor: 'rgb(54, 162, 235, 0.25)',
                             borderColor: 'rgb(54, 162, 235, 0.25)'
-                        }
+                        }}
                     ]
-                };
+                }};
 
                 var fullData = [            
-                    {                
+                    {{                
                         "degree": "This is a demo comment (1).",
                         "subject_code": "This is a demo comment (1).",
                         "subject_name": "This is a demo comment (1).",
                         "question_statement": "This is a demo comment (1).",
                         "value": "This is a demo comment (1).",
-                    },
-                    {                
+                    }},
+                    {{                
                         "degree": "This is a demo comment (2).",
                         "subject_code": "This is a demo comment (2).",
                         "subject_name": "This is a demo comment (2).",
                         "question_statement": "This is a demo comment (2).",
                         "value": "This is a demo comment (2).",
-                    },
-                    {                
+                    }},
+                    {{                
                         "degree": "This is a demo comment (3).",
                         "subject_code": "This is a demo comment (3).",
                         "subject_name": "This is a demo comment (3).",
                         "question_statement": "This is a demo comment (3).",
                         "value": "This is a demo comment (3).",
-                    }
+                    }}
                 ];
-
             
-                var globalChart = new Chart(document.getElementById('globalChart'), {
+                var globalChart = new Chart(document.getElementById('globalChart'), {{
                     type: 'polarArea',
                     data: globalData,                
-                    options: {                    
+                    options: {{                    
                         responsive: true,
                         maintainAspectRatio: false,
-                        plugins: {
-                            legend: {
+                        plugins: {{
+                            legend: {{
                                 //position: 'left'
                                 display: false
-                            }
-                        }
-                    },
-                });
+                            }}
+                        }}
+                    }},
+                }});
 
-                var totalChart = new Chart(document.getElementById('totalChart'), {
-                    type: 'line',
+                var totalChart = new Chart(document.getElementById('totalChart'), {{
+                    type: 'bar',
                     data: totalData,                
-                    options: {                    
+                    options: {{                    
                         responsive: true,
                         maintainAspectRatio: false,
-                        scales: {
-                            x:{
-                                title: {
+                        scales: {{
+                            x:{{
+                                title: {{
                                     display: true,
                                     text: 'Puntuació'
-                                }
-                            },
-                            y:{
-                                title: {
+                                }}
+                            }},
+                            y:{{
+                                title: {{
                                     display: true,
                                     text: 'Quantitat de valoracions'
-                                }
-                            }
-                        },
-                        plugins: {
-                            legend: {
+                                }}
+                            }}
+                        }},
+                        plugins: {{
+                            legend: {{
                                 //position: 'right'
                                 display: false
-                            }
-                        }
-                    },
-                });
+                            }}
+                        }}
+                    }},
+                }});
 
-                $('#table').DataTable({
+                $('#table').DataTable({{
                     data: fullData,
                     columns: [                                     
-                        {data: "value"}
+                        {{data: "value"}}
                     ]          
-                });
+                }});
 
-                var table = $('#data').DataTable({
+                var table = $('#data').DataTable({{
                     data: fullData,
                     columns: [                                     
-                        {data: "degree"},
-                        {data: "subject_code"},
-                        {data: "subject_name"},
-                        {data: "question_statement"},
-                        {data: "value"}
+                        {{data: "degree"}},
+                        {{data: "subject_code"}},
+                        {{data: "subject_name"}},
+                        {{data: "question_statement"}},
+                        {{data: "value"}}
                     ],
                     "columnDefs": [
-                        {
+                        {{
                             "targets": [ 1 ],
                             "visible": false,
                             "searchable": false
-                        }
+                        }}
                     ],
                     dom: 'Bfrtip',
                     buttons: [
                         'copy', 'excel', 'pdf'
                     ],            
-                });
+                }});
                 
                 var legendItems = document.querySelector('#legend').getElementsByTagName('li');
 
-                for (var i = 0; i < legendItems.length; i++) {
+                for (var i = 0; i < legendItems.length; i++) {{
                     legendItems[i].addEventListener("click", legendClickCallback.bind(this,i), false);
-                }
+                }}
 
-                function legendClickCallback(legendItemIndex){
+                function legendClickCallback(legendItemIndex){{
                     var legendItem = document.querySelector('#legend').getElementsByTagName('li')[legendItemIndex];
                     legendItem.classList.toggle("cross");
 
-                    document.querySelectorAll('canvas').forEach((chartItem,index)=>{
+                    document.querySelectorAll('canvas').forEach((chartItem,index)=>{{
                         var chart = Chart.instances[index];
                         if(chart.config.type == "polarArea") chart.toggleDataVisibility(legendItemIndex);
                         else  chart.data.datasets[legendItemIndex].hidden = !(chart.data.datasets[legendItemIndex].hidden ?? false);                    
                         chart.update();                    
-                    });  
-                }
-            };         
+                    }});  
+                }}
+            }};         
         </script>
     <head>
     
     <body>
         <header>
-            <h1>DAM</h1>
-            <h2>MP04: Llenguatges de marques i sistemes de gestió d'informació</h2>
+            <h1>{course_code}</h1>
+            <h2>{mp_code}: {mp_name}</h2>
         </header>
 
         <div class="box full">
